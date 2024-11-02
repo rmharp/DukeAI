88c80268c3902960702171e37d74d624fd7af7c7
mpuglise04 <mjpuglise@gmail.com>
2024-11-02 17:54:03 -0400

    Added role select page with states

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ import requests
 cred = credentials.Certificate('dukeai-103f8-369df2b50aa4.json')
 #firebase_admin.initialize_app(cred)
 
+def role_selection_page():
+    st.title("Select your position:")
+    
+    if st.button('Participant'):
+        st.write("You selected Participant.")
+    if st.button('Researcher'):
+        st.write("You selected Researcher.")
+
+
 def app():
 
     st.title(':blue[T]rial :blue[T]alk')
@@ -19,12 +28,15 @@ def app():
         st.session_state.username = ''
     if 'useremail' not in st.session_state:
         st.session_state.useremail = ''
-    
-    def f():
+    if 'show_role_selection' not in st.session_state:
+        st.session_state.show_role_selection = False 
+
+    def state_setup():
         try:
             user = auth.get_user_by_email(email)
             st.session_state.useremail = user.email
             st.session_state.username = user.uid
+            st.session_state.show_role_selection = True
             st.write('Login Succesful')
             #st.session_state.signout = True
             #st.session_state.signedout = True
@@ -32,9 +44,10 @@ def app():
         except:
             st.warning('Login failed please try again.')
             
-    def t():
+    def reset_states():
         st.session_state.signout = False
         st.session_state.signedout = False
+        st.session_state.show_role_selection = False
         st.session_state.username = ''
 
     if 'signedout' not in st.session_state:
@@ -42,30 +55,34 @@ def app():
     if 'signout' not in st.session_state:
         st.session_state.signout = False
         
-    if not st.session_state['signedout']:
+
+    if st.session_state.username and st.session_state.show_role_selection:
+        role_selection_page()
+    else:
+        # Login/Signup form
         choice = st.selectbox('Login/Signup', ['Login', 'Sign Up'])
     
         if choice == 'Login':
+            global email  # Make email accessible to the `state_setup` function
             email = st.text_input('Email Address')
-            password = st.text_input('Password', type = 'password')
-
-            st.button('Login', on_click=f)
+            password = st.text_input('Password', type='password')
+            st.button('Login', on_click=state_setup)
         else: 
             email = st.text_input('Email Address')
-            password = st.text_input('Password', type = 'password')
+            password = st.text_input('Password', type='password')
             username = st.text_input('Enter your unique username')
 
             if st.button('Create my account'):
-                user = auth.create_user(email = email, password = password, uid=username)
-                #user = sign_up_with_email_and_password(email=email,password=password,username=username)
-                
+                user = auth.create_user(email=email, password=password, uid=username)
                 st.success('Account created successfully!')
-                st.markdown('Please Login using your email and password')
+                st.markdown('Please login using your email and password')
                 st.balloons()
-    
+
     if st.session_state.signout:
         st.text('Username: ' + st.session_state.username)
         st.text('Email: ' + st.session_state.useremail)
-        st.button('Sign Out', on_click=t)
+        st.button('Sign Out', on_click=reset_states)
 
 app()
+
+# Choose between Participant and Researcher
