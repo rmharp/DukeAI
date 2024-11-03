2b4834184192961e04a9348c3482eca068ec0ee9
Riley Harper <riley.harper@unc.edu>
2024-11-03 06:09:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,19 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Initialize Firebase only if it hasn't been initialized yet
 if not firebase_admin._apps:
-    cred = credentials.Certificate('dukeai-103f8-369df2b50aa4.json')
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["firebase"]["universe_domain"]
+    })
     firebase_admin.initialize_app(cred)
 
     # Add custom CSS styling
